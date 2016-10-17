53a6915fc39fda3b42b1d31a106c54d8149b07f3
seveniu <prime3721@gmail.com>
2016-10-17 17:44:06 +0800

    优化线程池

--- src/main/java/com/seveniu/DataQueue.java
+++ src/main/java/com/seveniu/DataQueue.java
@@ -3,12 +3,11 @@ package com.seveniu;
 import com.alibaba.fastjson.JSON;
 import com.seveniu.consumer.Consumer;
 import com.seveniu.node.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -17,6 +16,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class DataQueue {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
             0, 20, 10, TimeUnit.MINUTES, new SynchronousQueue<>(),
@@ -28,7 +28,18 @@ public class DataQueue {
                     return new Thread(r, "data-process-thread-" + count.getAndIncrement());
                 }
             },
-            new ThreadPoolExecutor.CallerRunsPolicy());
+            new RejectedExecutionHandler() {
+                @Override
+                public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                    if (!executor.isShutdown()) {
+                        try {
+                            executor.getQueue().put(r);
+                        } catch (InterruptedException e) {
+                            // should not be interrupted
+                        }
+                    }
+                }
+            });
 
     public void start(String host, int port, final String key, Consumer consumer) {
         Jedis jedis = new Jedis(host, port);
@@ -36,13 +47,17 @@ public class DataQueue {
             @Override
             public void run() {
                 while (true) {
-                    String data = jedis.blpop(0, key).get(1);
-                    threadPoolExecutor.execute(new Runnable() {
-                        @Override
-                        public void run() {
-                            consumer.done(JSON.parseObject(data, Node.class));
-                        }
-                    });
+                    try {
+                        String data = jedis.blpop(0, key).get(1);
+                        threadPoolExecutor.execute(new Runnable() {
+                            @Override
+                            public void run() {
+                                consumer.done(JSON.parseObject(data, Node.class));
+                            }
+                        });
+                    } catch (Exception e) {
+                        logger.error("consumer data error : {}", e.getMessage());
+                    }
                 }
             }
         }, "get-data-from-queue-thread").start();
